51d549e918d1f22702314632c64ec503daf543b3
duankhoahoc <lenguyenngocthieu55@gmail.com>
2026-04-10 13:44:54 +0700

    tasks.js

--- api/tasks.js
+++ api/tasks.js
@@ -1,4 +1,3 @@
-// api/tasks.js - Quản lý bài tập
 export default async function handler(req, res) {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
