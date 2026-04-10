2a98f03374d6ab3f48743b20180b28d99eaac215
duankhoahoc <lenguyenngocthieu55@gmail.com>
2026-04-10 13:41:25 +0700

    classes.js

--- api/classes.js
+++ api/classes.js
@@ -1,4 +1,3 @@
-// api/classes.js - Quản lý lớp học
 export default async function handler(req, res) {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
