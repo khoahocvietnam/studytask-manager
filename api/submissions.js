daa556e66469280106091362796d5aa532f297f9
duankhoahoc <lenguyenngocthieu55@gmail.com>
2026-04-10 13:43:41 +0700

    submissions.js

--- api/submissions.js
+++ api/submissions.js
@@ -1,4 +1,3 @@
-// api/submissions.js - Quản lý bài nộp của học sinh
 export default async function handler(req, res) {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, OPTIONS');
