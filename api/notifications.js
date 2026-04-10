fbb114f22932703d7087e4efef71da7ec9b765cc
duankhoahoc <lenguyenngocthieu55@gmail.com>
2026-04-10 13:42:43 +0700

    notifications.js

--- api/notifications.js
+++ api/notifications.js
@@ -1,4 +1,3 @@
-// api/notifications.js - Quản lý thông báo
 export default async function handler(req, res) {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, DELETE, OPTIONS');
